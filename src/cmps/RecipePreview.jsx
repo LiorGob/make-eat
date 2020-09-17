43be48a5b1390eb1e5faa2d23166ec2103658d81
lior_ <liorgobari2@gmail.com>
2020-09-17 19:24:24 +0300

    stars

--- src/cmps/RecipePreview.jsx
+++ src/cmps/RecipePreview.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Stars from 'simple-rating-stars';
+// import Stars from 'simple-rating-stars';
+import GradeIcon from '@material-ui/icons/Grade';
 
 
 export function RecipePreview({ recipe }) {
@@ -12,7 +13,8 @@ export function RecipePreview({ recipe }) {
                </Link>
                 <div className="recipe-preview-txt">
                     <h3>{recipe.name}</h3>
-    <div><Stars stars={1} outOf={2} full={'#ffff00'} empty={'#E1F1FF'} stroke={'#369'} />{ratingAvg}({recipe.reviews.length})</div>
+    {/* <div><Stars stars={1} outOf={2} full={'#ffff00'} empty={'#E1F1FF'} stroke={'#369'} />{ratingAvg}({recipe.reviews.length})</div> */}
+    <div><GradeIcon style={{ color: '#ff385c' }}/>{ratingAvg}({recipe.reviews.length})</div>
                     <p>{recipe.createdBy.fullName}</p>
                 </div> 
         </div>
