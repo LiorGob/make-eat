849cdb88fae8bf29853aafdc36e3cee542043e06
lior_ <liorgobari2@gmail.com>
2020-09-19 09:30:13 +0300

    ditails and style

--- src/cmps/RecipeDirection.jsx
+++ src/cmps/RecipeDirection.jsx
@@ -1,21 +1,45 @@
-import React from 'react'
+import React, { Component } from 'react'
 import SpoonIcon from './SpoonIcon';
+import { RecipeDirectionStep } from './RecipeDirectionStep';
 
-export function RecipeDirection({ recipe }) {
-    return (
-        <div className="directions-recipe">
-            <SpoonIcon />
-            <span>Directions</span>
-            <ul>
-                {recipe.directions.map((direction, idx) => <li key={direction.id} direction={direction} >
-                    <label>
-                        <input type="checkbox" />
-                        <span>Step {idx + 1}</span>
-                    </label>
-                    <div><p>{direction}</p></div></li>)}
-            </ul>
-            <button><SpoonIcon /></button>
-        </div>
 
-    )
+export class RecipeDirection extends Component {
+
+    state = {
+        directions: []
+
+    }
+
+    componentDidMount() {
+        var directions = []
+        this.props.recipe.directions.map(() => {
+            directions.push(false)
+        })
+        this.setState({ directions })
+    }
+
+    onDirectionCheck = (idx) => {
+        var { directions } = this.state
+        directions[idx] = !directions[idx]
+        console.log('directions', directions)
+        this.setState({ directions: [...this.state.directions] })
+
+    }
+
+
+    render() {
+        const { recipe } = this.props
+        return (
+            <div className="direction-recipe" >
+                <SpoonIcon />
+                <span className="font-bold">Directions</span>
+                <ul>
+                    {recipe.directions.map((direction, idx) =>
+                        <RecipeDirectionStep key={idx} direction={direction} idx={idx} onDirectionCheck={this.onDirectionCheck} directionChecked={this.state.directions[idx]} />)}
+                </ul>
+                <button><SpoonIcon /> <span className="font-bold"> I Made It</span></button>
+            </div>
+
+        )
+    }
 }
---
+++ src/cmps/RecipeIngredient.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import DoneAllIcon from '@material-ui/icons/DoneAll';
+
+export  function RecipeIngredient({ recipe }) {
+    return (
+        <div className="ingredient-recipe">
+            <span className="font-bold">Ingredients:</span>
+            <ul>
+                {recipe.ingredients.map(ingredient => <li key={ingredient.produceId} ingredient={ingredient}>
+                    <label>
+                        <input type="checkbox" />
+                    </label>
+                    {ingredient.amount} {ingredient.spec ? ingredient.spec : ''} {ingredient.name}</li>)}
+                <button className="font-bold">Add All Ingredients To Shopping Cart
+                    <DoneAllIcon style={{ color: '#ff385c' }} />
+                    </button>
+            </ul>
+        </div>
+
+    )
+}
+
