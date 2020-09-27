65bccd20c6d6a037869ae10a9565629ed2b1b173
lior_ <liorgobari2@gmail.com>
2020-09-27 14:05:43 +0300

    deatils style and responsive

--- src/cmps/Review/AddReview.jsx
+++ src/cmps/Review/AddReview.jsx
@@ -7,7 +7,7 @@ class _AddReview extends Component {
 
     state = {
         recipe: {
-            reviews: [{ by:{}, txt: '', rating, }]
+            reviews: [{ by: {}, txt: '', rating, }]
         }
     }
 
@@ -23,10 +23,12 @@ class _AddReview extends Component {
         const field = target.name
         const value = (target.type === 'number') ? +target.value : target.value
         this.setState(prevState => {
+            var newReviews = [...this.state.reviews]
+            newReviews.push(value)
             return {
                 recipe: {
                     ...prevState.recipe,
-                    [field]: value
+                    reviews: newReviews
                 }
             }
         })
@@ -59,5 +61,58 @@ const mapDispatchToProps = {
 
 export const AddReview = connect(mapStateToProps, mapDispatchToProps)(_AddReview)
 
+
+
+
+
+
+// onInputChange = (ev) => {
+//     const value = ev.target.type === 'rating' ? +ev.target.value : ev.target.value
+//     this.setState({ reviewToAdd: { ...this.state.reviewToAdd, [ev.target.name]: value } })
+// }
+
+// addReview = (ev) => {
+//     ev.preventDefault();
+//     this.props.onAddReview(this.state.reviewToAdd);
+//     this.setState({reviewToAdd: bookService.getEmptyReview()})
+// }
+
+// render() {
+//     return (
+//         <div className="review-container">
+//             <label>Add Review:</label>
+//             <form className="from-review" onSubmit={ this.addReview }>
+//                 <input className="full-name-review" name="fullName" value={ this.state.reviewToAdd.fullName }
+//                     placeholder="Full Name" type="text"
+//                     onChange={ this.onInputChange } />
+
+//                 <input name="rating" value={ this.state.value }
+//                     min="1" max="5"
+//                     placeholder="Rate" type="range"
+//                     onChange={ this.onInputChange } />
+//                 <label>
+//                     <input type="date" id="datepicker" name="datepicker"
+//                         onChange={ this.onInputChange } />
+//                 </label>
+
+//                 <input className="free-text-review" name="txt" value={ this.state.txt }
+//                     placeholder="Free text" type="text" value={ this.state.reviewToAdd.txt }
+//                     onChange={ this.onInputChange } />
+//                 <button className="btn-add-review">Add Review</button>
+//             </form>
+//         </div>
+//     )
+// }
+
+
+// function addReview(recipeId, review) {
+//     return getById(recipeId)
+// 
+//         .then(book => {
+//             if (!book.reviews) book.reviews = [];
+//             book.reviews.push(review);
+//             storageService.saveToStorage(KEY_BOOKS, gBooks);
+//         })
+// }
 
 
